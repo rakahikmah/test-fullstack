8a84afb7e63a0eb22e2534967acb4224fd391694
rakahikmah46 <rakharamadhan@cashlez.com>
2022-09-02 13:39:21 +0700

    change format rupiah

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,18 @@
             <div class="card" style="height: 40rem">
                 <img class="card-img-top" src="{{ asset('docs/'.$value->image_name) }}" height="140px" width="70px" alt="Card image cap">
                 <div class="card-body">
+                  <p><b>{{$value->name_product}}</b> ({{$value->name}})</p>
                   <p class="card-text">
                         {!! $value->description !!}
                   </p>
                 </div>
                   <div class="card-body">
-                        <center><button type="button" class="btn btn-primary">Beli</button></center>
+                        <center>
+                            <p><b>Rp.{{number_format($value->price,0,',','.')}}</b></p>
+                        </center>
+                        <center>
+                            <button type="button" class="btn btn-primary">Beli</button>
+                        </center>
                   </div>
               </div>
         </div>
@@ -22,30 +28,7 @@
             {{ $product->links() }}
         </center>
        
-        {{-- <div class="col-3">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-              </div>
-        </div>
-        <div class="col-3">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-              </div>
-        </div>
-        <div class="col-3">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-              </div>
-        </div> --}}
+   
     </div>
 
 </div>
